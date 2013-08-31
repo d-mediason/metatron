adc9241e60f03ea5ab21e605be0c9ac5f5e0a817
manaten <manaten@manaten.net>
2013-08-31 16:11:02 +0900

    update extension

--- extension/src/metatoron.js
+++ extension/src/metatoron.js
@@ -1,5 +1,5 @@
 chrome.tabs.getSelected(null, function(tab) {
-	$('#pageUrl').text(tab.url);
+	$('#pageUrl').text('<a href="http://f.morikuma.org/tree?uri='+tab.url+'" target="_blank">'+tab.url+'</a>');
 	$('#pageTitle').text(tab.title);
 });
 $(function(){
