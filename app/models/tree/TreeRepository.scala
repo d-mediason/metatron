7b863569987ee2ff68134551d0d2d2b6f60350e7
瀬川 卓也 <TakuyaSegawa@sekawa-no-MacBook-Air.local>
2013-08-31 16:20:25 +0900

    いまのところでなんとかした

--- app/models/tree/TreeRepository.scala
+++ app/models/tree/TreeRepository.scala
@@ -1,18 +1,19 @@
 package models.tree
 
 import com.redis.RedisClient
-import play.api.libs.json.Json
+import play.api.libs.json.{JsObject, Json}
 
 class TreeRepository {
-//  val redis = new RedisClient("f.morikuma.org", 6379)
-//
-//  def register(parent: String, child: String) = {
-//
-//  }
-//
-//  private def registerParent(parent: String, child: String) = {
-//    redis.get(parent).map{jsonString =>
-//      val children = Json.parse(jsonString).as[Map[String, List[String]]]
-//    }
-//  }
+  val redis = new RedisClient("f.morikuma.org", 6379)
+
+  def register(parent: String, child: String) = {
+
+  }
+
+  private def registerParent(parent: String, child: String) = {
+    redis.get(parent).map{jsonString =>
+      val deta = Json.parse(jsonString).as[Map[String, List[String]]]
+      deta.get("children").map(_.::(List(child)))
+    }
+  }
 }
